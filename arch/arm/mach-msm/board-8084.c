4d3900ece7caf06365d05e507db65117cfe18fda
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-01-18 10:58:09 +0100

    Revert "intelli/msm_thermal v1 (faux123)"
    drivers/thermal/Kconfig: fix default prompt error
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    intelli/msm_thermal: initial coding for Qualcomm Snapdragon 800 devices
    
    an intelligent MSM thermal throttling kernel driver designed to replace the
    closed sourced thermald(aemon) from Qualcomm.
    
    The current driver can throttle the SOC via core frequency scaling or via
    core hotplug with user configurable sysfs entries to adjust the throttling
    temperature thresholds for each.
    
    Derived from Qualcomm's Open source repo.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Intelli_thermal: Note 3 KK kernel fix up (CAF update fixup as well)
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    intelli-thermal: use its own dedicated workqueue
    
    this should reduce the load on the shared global workqueue for other
    processes and also give its own unique variable name for cpu limit
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    intelli-thermal: increase sampling time to reduce sampling overhead
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    board-8084-hammerhead: initialize intelli_thermal engine
    
    Signed-off-by: Jean-Pierre Rasquin <yank555-lu@gmail.com> (reverted from commit 046e2477c33f324a185584bf6cd92870f60d6bba)

--- arch/arm/mach-msm/board-8084.c
+++ arch/arm/mach-msm/board-8084.c
@@ -132,11 +132,7 @@ void __init apq8084_add_drivers(void)
 	else
 		msm_clock_init(&apq8084_clock_init_data);
 	tsens_tm_init_driver();
-#ifdef CONFIG_INTELLI_THERMAL
-	msm_thermal_init(NULL);
-#else
 	msm_thermal_device_init();
-#endif
 #if defined(CONFIG_LCD_KCAL)
 	shamu_add_lcd_kcal_devices();
 #endif

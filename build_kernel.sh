70570a889a769f646fa49411f0818fb87bde715c
yank555-lu <yank555.lu@gmail.com>
2015-06-08 20:57:09 +0200

    repo: build tools - set selinux to permissive on boot
    Change-Id: I7921184337e1389570202e7cb0d4f9cc9778b426
    Signed-off-by: yank555-lu <yank555.lu@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -41,7 +41,7 @@ cd $KERNELDIR
 make -j `getconf _NPROCESSORS_ONLN` || exit 1
 
 echo ".............................................................Making new boot image............................................................"
-./buildtools/mkbootimg_dtb --kernel $KERNELDIR/arch/arm/boot/zImage-dtb --ramdisk $RAMFS_TMP.cpio.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M" --pagesize 2048 -o $KERNELDIR/boot.img
+./buildtools/mkbootimg_dtb --kernel $KERNELDIR/arch/arm/boot/zImage-dtb --ramdisk $RAMFS_TMP.cpio.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M selinux=0" --pagesize 2048 -o $KERNELDIR/boot.img
 
 echo "...............................................................Stripping Modules.............................................................."
 find . -name "*.ko" -exec mv {} . \;
